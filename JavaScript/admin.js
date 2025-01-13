9d4d9a782b3424f793058c3617cebfc98fb72dc4
Gabriel <gcr2@outlook.com.br>
2025-01-12 21:50:33 -0300

    Add files via upload

--- JavaScript/admin.js
+++ JavaScript/admin.js
@@ -1,6 +1,10 @@
+var tipoFiltro = 1; //1 para nome, 2 para email
+
 document.addEventListener("DOMContentLoaded", () => {
     atualizarLista();
     document.getElementById("form-cadastro").addEventListener("submit", cadastrar);
+
+    document.getElementById("filtrar-user").style.backgroundColor = "rgb(0, 170, 192)";
 });
 
 function cadastrar(){
@@ -8,7 +12,7 @@ function cadastrar(){
     const email = document.getElementById("email");
 
     localStorage.setItem(email.value, user.value);
-    atualizarLista
+    atualizarLista;
 }
 
 function atualizarLista(){
@@ -19,5 +23,46 @@ function atualizarLista(){
         const usuario = document.createElement("li");
         usuario.textContent = localStorage.getItem(localStorage.key(i)) + " - " + localStorage.key(i);
         listaUsuarios.appendChild(usuario);
+    }
+}
+
+function trocarTipoFiltro(tipo) {
+    tipoFiltro = tipo;
+    switch (tipoFiltro){
+        case 1:
+            document.getElementById("filtrar-user").style.backgroundColor = "rgb(0, 170, 192)";
+            document.getElementById("filtrar-email").style.backgroundColor = "rgb(170, 170, 170)";
+            break;
+        case 2:
+            document.getElementById("filtrar-email").style.backgroundColor = "rgb(0, 170, 192)";
+            document.getElementById("filtrar-user").style.backgroundColor = "rgb(170, 170, 170)";
+            break;
+    }
+}
+
+function filtrar(){
+    const listaUsuarios = document.getElementById("lista-usuarios");
+    let entrada = document.getElementById("barra-pesquisa");
+    listaUsuarios.innerHTML = "";
+
+    switch(tipoFiltro){
+        case 1:
+            for(let i = 0; i < localStorage.length; i++){
+                if(localStorage.getItem(localStorage.key(i)) == entrada.value){
+                    const usuario = document.createElement("li");
+                    usuario.textContent = localStorage.getItem(localStorage.key(i)) + " - " + localStorage.key(i);
+                    listaUsuarios.appendChild(usuario);
+                }
+            }
+            break;
+        case 2:
+            for(let i = 0; i < localStorage.length; i++){
+                if(localStorage.key(i) == entrada.value){
+                    const usuario = document.createElement("li");
+                    usuario.textContent = localStorage.getItem(localStorage.key(i)) + " - " + localStorage.key(i);
+                    listaUsuarios.appendChild(usuario);
+                }
+            }
+            break;
     }
 }
